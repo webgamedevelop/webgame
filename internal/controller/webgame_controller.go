9414b5ba4937f20fb2b72c3b0f6642d1e0c49c17
webgamedevelop <webgamedevelop@163.com>
2023-12-07 23:34:25 +0800

    implement custom controller

--- internal/controller/webgame_controller.go
+++ internal/controller/webgame_controller.go
@@ -1,28 +1,20 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controller
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	webgamev1 "github.com/webgamedevelop/webgame/api/v1"
@@ -42,8 +34,8 @@ type WebGameReconciler struct {
 // move the current state of the cluster closer to the desired state.
 func (r *WebGameReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
-	logger.Info("webgame event received")
-	defer func() { logger.Info("webgame event handling completed") }()
+	logger.V(2).Info("webgame event received")
+	defer func() { logger.V(2).Info("webgame event handling completed") }()
 
 	var webgame webgamev1.WebGame
 	if err := r.Get(ctx, req.NamespacedName, &webgame); err != nil {
@@ -56,7 +48,148 @@ func (r *WebGameReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		return ctrl.Result{}, err
 	}
 
-	logger.Info("show webgame name", "name", webgame.GetName())
+	selector := map[string]string{
+		"gameType": webgame.Spec.GameType,
+		"instance": webgame.GetName(),
+	}
+
+	// create deployment
+	var deployment = appsv1.Deployment{}
+	deployment.SetNamespace(webgame.GetNamespace())
+	deployment.SetName(webgame.GetName())
+	mutate := func() error {
+		deployment.SetLabels(labels.Merge(deployment.GetLabels(), webgame.GetLabels()))
+		deployment.Spec.Replicas = webgame.Spec.Replicas
+		deployment.Spec.Selector = &metav1.LabelSelector{MatchLabels: selector}
+		deployment.Spec.Template.SetLabels(labels.Merge(webgame.GetLabels(), selector))
+
+		container := corev1.Container{}
+		if len(deployment.Spec.Template.Spec.Containers) != 0 {
+			container = deployment.Spec.Template.Spec.Containers[0]
+		}
+		container.Name = webgame.GetName()
+		container.Image = webgame.Spec.Image
+		container.ImagePullPolicy = corev1.PullIfNotPresent
+		container.Resources = corev1.ResourceRequirements{}
+		container.Ports = []corev1.ContainerPort{{
+			Name:          "web",
+			ContainerPort: int32(webgame.Spec.ServerPort.IntValue()),
+			Protocol:      corev1.ProtocolTCP,
+		}}
+
+		deployment.Spec.Template.Spec.Containers = []corev1.Container{container}
+		return ctrl.SetControllerReference(&webgame, &deployment, r.Scheme)
+	}
+
+	res, err := ctrl.CreateOrUpdate(ctx, r.Client, &deployment, mutate)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if res != controllerutil.OperationResultNone {
+		logger.Info("deployment changed", "res", res)
+		return ctrl.Result{}, nil
+	}
+
+	// create service
+	var service = corev1.Service{}
+	service.SetNamespace(webgame.GetNamespace())
+	service.SetName(webgame.GetName())
+	mutate = func() error {
+		service.SetLabels(labels.Merge(service.GetLabels(), webgame.GetLabels()))
+		service.Spec.Selector = selector
+		service.Spec.Type = corev1.ServiceTypeClusterIP
+		service.Spec.Ports = []corev1.ServicePort{{
+			Name:       "web",
+			Port:       int32(webgame.Spec.ServerPort.IntValue()),
+			TargetPort: webgame.Spec.ServerPort,
+			Protocol:   corev1.ProtocolTCP,
+		}}
+		return controllerutil.SetControllerReference(&webgame, &service, r.Scheme)
+	}
+
+	res, err = ctrl.CreateOrUpdate(ctx, r.Client, &service, mutate)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if res != controllerutil.OperationResultNone {
+		logger.Info("service changed", "res", res)
+		return ctrl.Result{}, nil
+	}
+
+	// create ingress
+	var (
+		ingress     = networkingv1.Ingress{}
+		pathType    = networkingv1.PathTypePrefix
+		path        = fmt.Sprintf("/%s/%s", selector["gameType"], selector["instance"])
+		rewriteRule = fmt.Sprintf(`rewrite ^%s/(.*)$ /$1 break;`, path)
+		annotations = map[string]string{
+			"nginx.ingress.kubernetes.io/configuration-snippet": rewriteRule,
+		}
+	)
+
+	ingress.SetNamespace(webgame.GetNamespace())
+	ingress.SetName(webgame.GetName())
+	mutate = func() error {
+		ingress.SetLabels(labels.Merge(ingress.GetLabels(), webgame.GetLabels()))
+		ingress.SetAnnotations(labels.Merge(ingress.GetAnnotations(), annotations))
+		ingress.Spec = networkingv1.IngressSpec{
+			IngressClassName: &webgame.Spec.IngressClass,
+			Rules: []networkingv1.IngressRule{{
+				IngressRuleValue: networkingv1.IngressRuleValue{
+					HTTP: &networkingv1.HTTPIngressRuleValue{
+						Paths: []networkingv1.HTTPIngressPath{{
+							PathType: &pathType,
+							Path:     path,
+							Backend: networkingv1.IngressBackend{
+								Service: &networkingv1.IngressServiceBackend{
+									Name: service.GetName(),
+									Port: networkingv1.ServiceBackendPort{
+										Number: int32(webgame.Spec.ServerPort.IntValue()),
+									},
+								},
+							},
+						}},
+					},
+				},
+			}},
+		}
+
+		return controllerutil.SetControllerReference(&webgame, &ingress, r.Scheme)
+	}
+
+	res, err = ctrl.CreateOrUpdate(ctx, r.Client, &ingress, mutate)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if res != controllerutil.OperationResultNone {
+		logger.Info("ingress changed", "res", res)
+		return ctrl.Result{}, nil
+	}
+
+	logger.Info("sync status")
+	mutate = func() error {
+		index := strings.TrimPrefix(webgame.Spec.IndexPage, "/")
+		path := strings.TrimPrefix(path, "/")
+		address := fmt.Sprintf("%s/%s/%s", webgame.Spec.Domain, path, index)
+		webgame.Status.DeploymentStatus = *deployment.Status.DeepCopy()
+		webgame.Status.GameAddress = address
+		webgame.Status.ClusterIP = service.Spec.ClusterIP
+		return nil
+	}
+
+	// update webgame status
+	res, err = controllerutil.CreateOrPatch(ctx, r.Client, &webgame, mutate)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if res != controllerutil.OperationResultNone {
+		// non-return
+		logger.Info("webgame status synced")
+	}
 	return ctrl.Result{}, nil
 }
 
@@ -64,5 +197,8 @@ func (r *WebGameReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 func (r *WebGameReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&webgamev1.WebGame{}).
+		Owns(&appsv1.Deployment{}).
+		Owns(&corev1.Service{}).
+		Owns(&networkingv1.Ingress{}).
 		Complete(r)
 }
